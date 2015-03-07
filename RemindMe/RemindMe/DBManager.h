307d3f847b053031cc1a548a69a6416da00d0efe
Ankita Kalangutkar <kalangutkar3ankita3v@gmail.com>
2015-03-07 12:39:51 +0530

    created a singleton class

--- RemindMe/RemindMe/DBManager.h
+++ RemindMe/RemindMe/DBManager.h
@@ -7,7 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+@import CoreData;
 
 @interface DBManager : NSObject
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
++ (instancetype)sharedInstance;
 
 @end
