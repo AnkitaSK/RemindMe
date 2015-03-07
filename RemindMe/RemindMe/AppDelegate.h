226375c359a3d7c3ef501578cb2753b4dc539e65
Ankita Kalangutkar <kalangutkar3ankita3v@gmail.com>
2015-03-07 11:52:06 +0530

    initial commit

---
+++ RemindMe/RemindMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RemindMe
+//
+//  Created by Ankita Kalangutkar on 3/7/15.
+//  Copyright (c) 2015 Ankita Kalangutkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RemindMe/RemindMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemindMe
+//
+//  Created by Ankita Kalangutkar on 3/7/15.
+//  Copyright (c) 2015 Ankita Kalangutkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
