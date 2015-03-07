7fa6a2940b7e5cf934c9db6fe0b36342afdf7819
Ankita Kalangutkar <kalangutkar3ankita3v@gmail.com>
2015-03-07 13:38:38 +0530

    created a drawer view in a storyboard

---
+++ RemindMe/RemindMe/CenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.h
+//  RemindMe
+//
+//  Created by Ankita Kalangutkar on 3/7/15.
+//  Copyright (c) 2015 Ankita Kalangutkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterViewController : UIViewController
+
+@end
