d510b867b6c0a941eb6234c7439e5fe1af8fbe39
Ankita Kalangutkar <kalangutkar3ankita3v@gmail.com>
2015-03-07 12:02:01 +0530

    created a DBManager class

---
+++ RemindMe/RemindMe/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  RemindMe
+//
+//  Created by Ankita Kalangutkar on 3/7/15.
+//  Copyright (c) 2015 Ankita Kalangutkar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ RemindMe/RemindMe/DBManager.m
@@ -0,0 +1,13 @@
+//
+//  DBManager.m
+//  RemindMe
+//
+//  Created by Ankita Kalangutkar on 3/7/15.
+//  Copyright (c) 2015 Ankita Kalangutkar. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+
+@end
