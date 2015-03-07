13e197ea031bb6a0ea5aa3ce2bfe8267376e14df
Ankita Kalangutkar <kalangutkar3ankita3v@gmail.com>
2015-03-07 12:49:21 +0530

    created entities created managed object class

---
+++ RemindMe/RemindMe/CalenderData.m
@@ -0,0 +1,19 @@
+//
+//  CalenderData.m
+//  RemindMe
+//
+//  Created by Ankita Kalangutkar on 3/7/15.
+//  Copyright (c) 2015 Ankita Kalangutkar. All rights reserved.
+//
+
+#import "CalenderData.h"
+
+
+@implementation CalenderData
+
+@dynamic day;
+@dynamic weekday;
+@dynamic month;
+@dynamic year;
+
+@end
---
+++ RemindMe/RemindMe/CustomerInfo.m
@@ -0,0 +1,16 @@
+//
+//  CustomerInfo.m
+//  RemindMe
+//
+//  Created by Ankita Kalangutkar on 3/7/15.
+//  Copyright (c) 2015 Ankita Kalangutkar. All rights reserved.
+//
+
+#import "CustomerInfo.h"
+
+
+@implementation CustomerInfo
+
+@dynamic customerName;
+
+@end
