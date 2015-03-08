f80a632bff043feb9a91aa13a3c2268c9750551e
Ankita Kalangutkar <kalangutkar3ankita3v@gmail.com>
2015-03-08 11:08:54 +0530

    displayed in a left panel view

--- RemindMe/RemindMe/LeftPanelViewController.m
+++ RemindMe/RemindMe/LeftPanelViewController.m
@@ -8,7 +8,8 @@
 
 #import "LeftPanelViewController.h"
 
-@interface LeftPanelViewController ()
+@interface LeftPanelViewController ()<UITableViewDataSource,UITabBarDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *tableviewCustomer;
 
 @end
 
@@ -16,7 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+//    connect to parse and fetch from parse
+    
+//    store it in a db
+    
+//    fetch it from db
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,6 +30,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 5;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = @"test";
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
 /*
 #pragma mark - Navigation
 
